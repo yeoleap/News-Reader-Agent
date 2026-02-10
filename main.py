ac9ab29a7c31441207c1da7a174a297aaf7fffcd
dayeol-jo_ktdev <dayeol.jo@kt.com>
2026-02-10 18:14:12 +0900

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from news-reader-agent!")
+import dotenv
 
+dotenv.load_dotenv()
 
-if __name__ == "__main__":
-    main()
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
+
+@CrewBase
+class TranslatorCrew:
+    
+    # agent 를 만드려면 함수에 agent decorator 를 붙여야 한다.
+    @agent
+    def translator_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config["translator_agent"]
+        )
+
+    @task
+    def translate_task(self):
+        return Task(
+            config=self.tasks_config["translate_task"]
+        )
+    @task
+    def retranslate_task(self):
+        return Task(
+            config=self.tasks_config["retranslate_task"]
+        )   
+    @crew
+    def assemble_crew(self):
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            # Enable verbose mode for detailed logging
+            verbose=True
+        )
+    
+TranslatorCrew().assemble_crew().kickoff(inputs={"sentence": "I'm daeyeol and I like to ride my bicicle in napoli"})
